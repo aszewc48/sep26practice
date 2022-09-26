512ac4c9dd33a10cea2a02beb0caee65df7ecdc1
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-09-26 11:09:35 -0400

    created react app and practiced with components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+
+    const myName = 'Jack'
+
+    return (
+        <header className="App-header">
+        <h2>Hello {myName}!</h2>
+        <img src="https://i.imgur.com/8uPMJOs.png" className="App-logo" alt="logo" />
+        </header>
+)
+}
+
+export default Header
